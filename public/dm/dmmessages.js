407547539e204a247b8826d9c4f388c5d19038c2
Chaos-Zero <ko_king_1@hotmail.com>
2022-08-15 00:47:38 +0100

    Explicitly specify string in footer

--- public/dm/dmmessages.js
+++ public/dm/dmmessages.js
@@ -591,10 +591,10 @@ function JargonMessage() {
         inline: false
       }
     )
-    .setFooter([
-      "danimyuu ♡",
-      "https://cdn.glitch.com/37568bfd-6a1d-4263-868a-c3b4d503a0b1%2FMewditto.png?v=1609471789850"
-    ]);
+    .setFooter({
+      text: "danimyuu ♡".toString(),
+      iconURL: "https://cdn.glitch.com/37568bfd-6a1d-4263-868a-c3b4d503a0b1%2FMewditto.png?v=1609471789850".toString()}
+    );
   return Embed;
 }
 
